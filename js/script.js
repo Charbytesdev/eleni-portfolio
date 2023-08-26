79c199680e6ff4a815f8da829561357d5f39a7f1
Charbytesdev <138222011+Charbytesdev@users.noreply.github.com>
2023-08-26 15:44:05 +0300

    Underline the active page and add button functionality in nav-bar

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 const mouseTracker = document.getElementById("mouse-tracker");
 document.body.addEventListener("pointermove", (e) => {
   const { clientX, clientY } = e;
-
   mouseTracker.animate(
     {
       left: `${clientX}px`,
@@ -9,4 +8,22 @@ document.body.addEventListener("pointermove", (e) => {
     },
     { duration: 3000, fill: "forwards" }
   );
+});
+
+const publications = document.getElementById("publications");
+
+const pageContainer = document.getElementById("page-container");
+
+const home = document.getElementById("home");
+
+document.addEventListener("wheel", (e) => {
+  pageContainer.scrollLeft > pageContainer.clientWidth;
+
+  if (pageContainer.scrollLeft > pageContainer.clientWidth / 2) {
+    home.classList.remove("active-page");
+    publications.classList.add("active-page");
+  } else {
+    home.classList.add("active-page");
+    publications.classList.remove("active-page");
+  }
 });

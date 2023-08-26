69cab429af910ddc2fb2d75dbfb362824412396b
Charbytesdev <138222011+Charbytesdev@users.noreply.github.com>
2023-08-26 17:41:37 +0300

    Connect navigation links to pages

--- js/script.js
+++ js/script.js
@@ -15,15 +15,32 @@ const publications = document.getElementById("publications");
 const pageContainer = document.getElementById("page-container");
 
 const home = document.getElementById("home");
+const resume = document.getElementById("resume");
+const about = document.getElementById("about");
 
 document.addEventListener("wheel", (e) => {
+  console.log(pageContainer.scrollLeft);
   pageContainer.scrollLeft > pageContainer.clientWidth;
 
-  if (pageContainer.scrollLeft > pageContainer.clientWidth / 2) {
+  if (pageContainer.scrollLeft > pageContainer.clientWidth * 2.5) {
+    home.classList.remove("active-page");
+    publications.classList.remove("active-page");
+    resume.classList.remove("active-page");
+    about.classList.add("active-page");
+  } else if (pageContainer.scrollLeft > pageContainer.clientWidth * 1.5) {
+    home.classList.remove("active-page");
+    publications.classList.remove("active-page");
+    resume.classList.add("active-page");
+    about.classList.remove("active-page");
+  } else if (pageContainer.scrollLeft > pageContainer.clientWidth / 2) {
     home.classList.remove("active-page");
     publications.classList.add("active-page");
+    resume.classList.remove("active-page");
+    about.classList.remove("active-page");
   } else {
     home.classList.add("active-page");
     publications.classList.remove("active-page");
+    resume.classList.remove("active-page");
+    about.classList.remove("active-page");
   }
 });
